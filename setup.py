746e411f75e3eaf2386aea03df7f37d47f3faf0b
Kushaan Gupta <gkushaan@rediffmail.com>
2025-03-08 11:51:30 -0500

    Update setup.py to support submodule imports

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
 setup(
-    name = 'linear2ac',
-    version = '0.0.1',
-    author = 'Johan Winnubst',
-    author_email = 'johan@e11.bio',
-    packages = ['linear2ac'],
-    install_requires=['umap-learn','fpdf2','plotly','figrid'],
-    zip_safe = False
+    name='linear2ac',
+    version='0.0.1',
+    author='Johan Winnubst',
+    author_email='johan@e11.bio',
+    packages=find_packages('linear2ac'),  # Automatically find all submodules
+    install_requires=['umap-learn', 'fpdf2', 'plotly', 'figrid'],
+    zip_safe=False
 )
