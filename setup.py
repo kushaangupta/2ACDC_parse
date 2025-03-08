8b00fb4765c895c2f2c74cb6bc41e70329803052
Kushaan Gupta <gkushaan@rediffmail.com>
2025-03-08 11:54:51 -0500

    retry fixing imports

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.1',
     author='Johan Winnubst',
     author_email='johan@e11.bio',
-    packages=find_packages('linear2ac'),  # Automatically find all submodules
+    packages=find_packages(include=['linear2ac', 'linear2ac.*']),  # Include all submodules
     install_requires=['umap-learn', 'fpdf2', 'plotly', 'figrid'],
     zip_safe=False
 )
